3442698817e9153b885ff881fff3d5706e0901e6
wangxiaolong <wangxiaolong@zhongan.com>
2018-07-05 20:19:00 +0800

    dsd

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
+
+var yargs = require('yargs')
+const argv = require('yargs').argv
+let { m = "info" } = argv
 const { exec } = require('child_process');
 exec('git add .',(error)=>{
     if(error){
         console.error(`exec add error: ${error}`)
         return
     }
-    exec('git commit -m d',(error)=>{
+    exec(`git commit -m "${m}"`,(error)=>{
         if(error){
             console.error(`exec commit error: ${error}`)
             return
         }
+        console.log(`commit "${m}"`)
         exec('git pull',(error,stdout)=>{
             if(error){
                 console.error(`exec pull error: ${error}`)
