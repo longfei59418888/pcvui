3ed2d37c77a8d3a0f501432586014ca5b3795bfa
wangxiaolong <wangxiaolong@zhongan.com>
2018-07-05 19:36:09 +0800

    d

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 const { exec } = require('child_process');
-exec('git add .',(error,stdout,stderr)=>{
+exec('git add .',(error)=>{
     if(error){
         console.error(`exec add error: ${error}`)
         return
     }
     exec('git commit -m d',(error,stdout,stderr)=>{
+        console.log(error,stdout,stderr)
         if(error){
             console.error(`exec commit error: ${error}`)
             return
