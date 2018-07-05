0e9398fb6b873e290062d9fa4ba63cb42058301d
wangxiaolong <wangxiaolong@zhongan.com>
2018-07-05 19:46:52 +0800

    d

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ exec('git add .',(error)=>{
                 console.error(`exec pull error: ${error}`)
                 return
             }
-            if(stdout == "Already up to date."){
+            if(stdout == "Already up to date"){
                 console.log(stdout)
             }
 
