a43693a1b8f856bca3d00513c1a239583e589cc3
wangxiaolong <wangxiaolong@zhongan.com>
2018-07-05 19:30:08 +0800

    test

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 const { spawn } = require('child_process');
-const ls = spawn('git', ['add', '.']);
-ls.stderr.on('data', (data) => {
+const add = spawn('git', ['add', '.']);
+add.stderr.on('data', (data) => {
     console.log(`add - 错误：${data}`);
 });
-ls.on('close', (code) => {
+add.on('close', (code) => {
     if(code == 0){
-        let lss = spawn('git', ['commit', '-m',"test"]);
-        lss.stderr.on('data', (data) => {
+        let commit = spawn('git', ['commit', '-m',"test"]);
+        commit.stderr.on('data', (data) => {
             console.log(`commit-错误：${data}`);
         });
-        lss.on('close', (code) => {
+        commit.on('close', (code) => {
             console.log(`commit-子进程退出码：${code}`);
         });
     }
